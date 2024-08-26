6ef7928409dcea433ecfc7a4e6e6ebf6f75f8817
ManDeep Singh <mandeeparora132@gmail.com>
2024-08-27 01:02:03 +0530

    Enable redux store in store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -28,7 +28,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  devTools: process.env.NODE_ENV !== "production",
+  // devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
