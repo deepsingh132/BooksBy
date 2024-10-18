c203e08a5910df3a93c6c45fcc4821811acf11b2
Mandeep Singh <mandeeparora132@gmail.com>
2024-10-18 17:26:41 +0530

    Add localhost to the cors origin

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["https://booksby.onrender.com", FRONTEND_URL],
+    origin: ["https://booksby.onrender.com", FRONTEND_URL, "http://localhost"],
   })
 );
 
